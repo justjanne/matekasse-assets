e87633d6ce4a22382d0e22c26c03c431d985eb60
Janne Mareike Koschinski <mail@justjanne.de>
2025-08-19 09:34:02 +0200

    Revert "chore: update readme"
    This reverts commit 97d017d3d63db6f4948cf4a25026687035878b70.

--- generate.sh
+++ generate.sh
@@ -15,7 +15,7 @@ for brand in $(find . -maxdepth 1 -type d -iname "[a-z0-9]*" | sort | xargs -n1 
 	for size in $(ls $brand | awk -F '-' '{print $NF}' | cut -d '.' -f 1 | cut -d '@' -f 1 | sort -nu); do
 		echo "### ${size}ml" >> README.md
 		find "_composite" -iname "$brand-*" -iname "*-$size.png" | sort | sed -E 's#^(.*)$#<img src="\1" width="64" height="64" />\&nbsp;#' >> README.md
-		find "$brand" -iname "*-$size.png" | sort | sed -E 's#^(.*).png$#<a href="\1@2.png"><img src="\1.png" width="64" height="64" /></a>\&nbsp;#' >> README.md
+		find "$brand" -iname "*-$size.png" | sort | sed -E 's#^(.*)$#<img src="\1" width="64" height="64" />\&nbsp;#' >> README.md
 		echo >> README.md
 	done
 done
